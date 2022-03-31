512203ecc581625ab373849d63e78968ceb0035a
muthu <muthukrrish333@gmail.com>
2022-03-31 22:22:16 +0530

    added Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import { Carousel } from './carousel/Carousel';
+import Header from './header/Header';
+// import CpcNavigation from './header/Header';
 // import Carousel from "./carousel/Carousel";
 
 function App() {
   return (
     <div className="App">
+     <Header />
       <Carousel />
     </div>
   );
--- src/carousel/Carousel.js
+++ src/carousel/Carousel.js
@@ -6,31 +6,45 @@ import "react-animated-slider/build/horizontal.css";
 import "./custom.css";
 import "./styles.css";
 
+import img1 from  "../assets/carousel/C1.jpg";
+import img2 from  "../assets/carousel/C2.jpg";
+import img3 from  "../assets/carousel/C3.jpg";
+import img4 from  "../assets/carousel/C4.jpg";
+
 const content = [
   {
-    title: "Vulputate Mollis Ultricies Fermentum Parturient",
-    description:
-      "Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Cras justo odio, dapibus ac facilisis.",
-    button: "Read More",
-    image: "https://i.imgur.com/ZXBtVw7.jpg",
+    // title: "Vulputate Mollis Ultricies Fermentum Parturient",
+    // description:
+      // "Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Cras justo odio, dapibus ac facilisis.",
+    button: "Get Started",
+    image: img1,
     user: "Luan Gjokaj",
     userProfile: "https://i.imgur.com/JSW6mEk.png"
   },
   {
-    title: "Tortor Dapibus Commodo Aenean Quam",
-    description:
-      "Nullam id dolor id nibh ultricies vehicula ut id elit. Cras mattis consectetur purus sit amet fermentum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec sed odio dui.",
-    button: "Discover",
-    image: "https://i.imgur.com/DCdBXcq.jpg",
+    // title: "Tortor Dapibus Commodo Aenean Quam",
+    // description:
+    //   "Nullam id dolor id nibh ultricies vehicula ut id elit. Cras mattis consectetur purus sit amet fermentum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec sed odio dui.",
+    button: "Get Started",
+    image: img2,
     user: "Erich Behrens",
     userProfile: "https://i.imgur.com/0Clfnu7.png"
   },
   {
-    title: "Phasellus volutpat metus",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Duis mollis, est non commodo luctus, nisi erat porttitor ligula.",
+    // title: "Phasellus volutpat metus",
+    // description:
+    //   "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Duis mollis, est non commodo luctus, nisi erat porttitor ligula.",
     button: "Buy now",
-    image: "https://i.imgur.com/DvmN8Hx.jpg",
+    image: img3,
+    user: "Bruno Vizovskyy",
+    userProfile: "https://i.imgur.com/4KeKvtH.png"
+  },
+  {
+    // title: "Phasellus volutpat metus",
+    // description:
+    //   "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Duis mollis, est non commodo luctus, nisi erat porttitor ligula.",
+    button: "Get Started",
+    image: img4,
     user: "Bruno Vizovskyy",
     userProfile: "https://i.imgur.com/4KeKvtH.png"
   }
@@ -38,7 +52,7 @@ const content = [
 
 export const Carousel = () => (
     <div className="wrapper">
-      <h1>react-animated-slider</h1>
+      <h1>Moira Buildings</h1>
     <Slider className="slider-wrapper">
       {content.map((item, index) => (
         <div
